c16882d34465a35488b02abf2a9e2c102eb7e075
Guru <guruprasad2511@gmail.com>
2021-06-30 13:46:19 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import  SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -13,6 +19,13 @@ function Header() {
                 </div>
             </div>
             <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="http://gurupra5ad.herokuapp.com/static/img/mobile.jpg" title="Me" />
+
 
             </div>
         </div>
